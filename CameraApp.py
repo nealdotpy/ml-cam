23c6d9f69512eef7e634a283c77b7558c6f8e99e
neal-rpi-zero <neal.rpi@gmail.com>
2019-12-16 14:56:02 -0500

    beautified code and full basic func'lity

--- CameraApp.py
+++ CameraApp.py
@@ -35,7 +35,16 @@ class CameraFrame(Frame): # "extends" Frame
 class GVObject(): # GoogleVisionObject
     def __init__(self, path):
         self.google_vision(path)
-        
+
+    def info(self):
+        print("FOUND: " + str(self.num_obj))
+        if (self.num_obj > 0): # don't try to parse if 0 objects found
+            for outer_key in self.found:
+                print("\n" + str(self.found[outer_key]["name"]) + ": "
+                      + str(round(self.found[outer_key]["score"]*100,2)) + " PERCENT")
+                print("N(x,y): (" + str(round(self.found[outer_key]["locx"], 5))
+                      + "," + str(round(self.found[outer_key]["locy"], 5)) + ")")
+
     def google_vision(self, path):
         """
         Localize objects in the local image.
@@ -49,34 +58,30 @@ class GVObject(): # GoogleVisionObject
             image = vision.types.Image(content=content)
             objects = client.object_localization(
                 image=image).localized_object_annotations
-            print('Number of objects found: {}'.format(len(objects)))
+#            print('Number of objects found: {}'.format(len(objects)))
             self.num_obj = len(objects) # num of objects
-            self.found = {}
+            self.found = {0 : {"test" : 69420}}
+            n = 0
             for object_ in objects:
-                print('\n{} (confidence: {})'.format(object_.name, object_.score))
-
-                print('Normalized bounding polygon vertices: ')
+                locx = -1 # can't be negative
+                locy = -1 # can't be negative
+#                print('\n{} (confidence: {})'.format(object_.name, object_.score))
+#                print('Normalized bounding polygon vertices: ')
+                i = 0
                 for vertex in object_.bounding_poly.normalized_vertices:
+                    if (locx < 0 and i % 2 == 0): # grabs vertex one which we want
+                        locx = vertex.x
+                        locy = vertex.y
+                    elif (locx > 0 and i % 2 == 0): # grabs vertex three which we want
+                        locx -= vertex.x
+                        locy -= vertex.y
                     print(' - ({}, {})'.format(vertex.x, vertex.y))
-                    
-#                found["name"] = {["confidence"] = object_.score}
-                    
-def display_img():
-    if (root != None):
-#        print("root is not None")
-        top = Toplevel(root)
-        ImageFrame(top).pack(side=LEFT)
-        
-def main():
-    global root
-    root = Tk() # root of application
-    root.title("PICAMERA by NEALDOTPY")
-    CameraFrame(root).pack(fill="both", expand=True)
-#    for i in range(0):
-#        print("instantiated a new toplevel.")
-#        top = Toplevel(root)
-#        ImageFrame(top).pack(fill="both", expand=True)
-    root.mainloop()
+                    i += 1
+                self.found[n] = {"name" : object_.name,
+                                 "score" : object_.score,
+                                 "locx" : abs(locx),
+                                 "locy" : abs(locy)}
+                n += 1
 
 def cap_preview():
     cam = PiCamera()
@@ -85,13 +90,26 @@ def cap_preview():
         cam.annotate_text = str(5 - i)
         sleep(1)
     cam.annotate_text = ""
-    print("captured pic. saved to 'pwd'")
+    print("captured pic. saved to 'pwd' as 'test_img.jpg'")
     cam.capture("/home/pi/Desktop/pi-proj/test_img.jpg")
     cam.stop_preview()
     cam.close()
     display_img()
     gvo = GVObject("test_img.jpg")
-#    google_vision("test_img.jpg") # analyze
+    gvo.info()
+
+def display_img():
+    if (root != None):
+        top = Toplevel(root)
+        ImageFrame(top).pack(side=LEFT)
+        
+def main():
+    global root
+    root = Tk() # root of application
+    root.title("PICAMERA by NEALDOTPY")
+    CameraFrame(root).pack(fill="both", expand=True)
+    cap_preview()
+    root.mainloop()    
 
 if __name__ == "__main__":
     main()
