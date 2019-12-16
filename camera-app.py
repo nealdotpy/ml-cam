b3a2e3b4a047f1c88c5609644fd1242b32f18b47
neal-rpi-zero <neal.rpi@gmail.com>
2019-12-16 13:11:42 -0500

    cleaned up code. working on post-capture display_img bug.

--- camera-app.py
+++ camera-app.py
@@ -9,7 +9,7 @@ import os, os.path
 from PIL import Image
 from PIL import ImageTk
 
-img_view = None
+root = None # global scope
 
 class ImageFrame(Frame): # "extends" Frame
     def __init__(self, parent): # 1 param constructor: "parent"
@@ -18,39 +18,34 @@ class ImageFrame(Frame): # "extends" Frame
         self.image_view = Label(self)
         self.image_view.image = self.img
         self.image_view.configure(image=self.img)
-        self.image_view.pack(side=LEFT)#"left", fill="both", expand=True)
+        self.image_view.pack(side=LEFT)
         parent.geometry("800x400")
-#        self.text = Label(self, text="word goes here").pack(side="left", fill="both", expand=True)
+
+class CameraFrame(Frame): # "extends" Frame
+    def __init__(self, parent): # 1 param constructor: "parent"
+        Frame.__init__(self, parent) # "super" call
+        preview = Button(self, text="TAKE PICTURE", fg="white", bg="green",
+                         font=font.Font(family='Helvetica', size=25, weight='bold'),
+                         command=cap_preview).pack()
+        stop = Button(self, text="EXIT", fg="white", bg="red",
+                      font=font.Font(family='Helvetica', size=20, weight='bold'),
+                      command=lambda : exit(0)).pack()
+        parent.geometry("800x400")
+
+def display_img():
+    if (root != None):
+        print("root is not None")
+        top = Toplevel(root)
+        ImageFrame(top).pack(side=LEFT)
         
 def main():
     root = Tk() # root of application
     root.title("PICAMERA by NEALDOTPY")
-#    text = Label(root, text="word goes here").pack(side="left", fill="both", expand=True)
-    ImageFrame(root).pack(fill="both", expand=True)
-#    img = ImageTk.PhotoImage(Image.open("test_img.jpg"))
-#    image_view = Label(root, image=img)
-#    image_view.pack(side="left", fill="both", expand=True)
-    for i in range(0):
-        print("instantiated a new toplevel.")
-        top = Toplevel(root)
-        ImageFrame(top).pack(fill="both", expand=True)
-    '''
-    view = Frame(root) # assign to root of application -- master
-    view.pack()
-    load = Image.open("test_img.jpg")
-    image = ImageTk.PhotoImage(load)
-    img_view = Label(view, text="IMAGE HERE", image=image)
-    preview = Button(view, text="TAKE PICTURE", fg="white", bg="green",
-                     #                 width=100, height=50,
-                     font=font.Font(family='Helvetica', size=25, weight='bold'),
-                     command=cap_preview).pack()
-    stop = Button(view, text="EXIT", fg="white", bg="red",
-                  #                 width=100, height=50,
-                  font=font.Font(family='Helvetica', size=20, weight='bold'),
-                  command=exit_app).pack()
-    if (img_view != None):
-        img_view.pack()        
-    '''
+    CameraFrame(root).pack(fill="both", expand=True)
+#    for i in range(0):
+#        print("instantiated a new toplevel.")
+#        top = Toplevel(root)
+#        ImageFrame(top).pack(fill="both", expand=True)
     root.mainloop()
 
 def googlevision(path):
@@ -77,7 +72,6 @@ def googlevision(path):
             print(' - ({}, {})'.format(vertex.x, vertex.y))
 
 def cap_preview():
-#    global count_label
     cam = PiCamera()
     cam.start_preview()
     for i in range(5):
@@ -88,30 +82,8 @@ def cap_preview():
     cam.capture("/home/pi/Desktop/pi-proj/test_img.jpg")
     cam.stop_preview()
     cam.close()
-    update_view()
-    sleep(0.000001) # microsecond
-    googlevision("test_img.jpg") # analyze
-
-def exit_app():
-    #googlevision("test_img_2.jpg") # easy testing
-    exit(0)
-
-def update_view():
-    print("Updating view. img_view exists: " + str(img_view != None))
-    display_out_img()
-
-def display_out_img():
-    global img_view
-    root = TopLevel()
-    root.title("DISPLAY_OUT")
-    root.geometry("800x400")
-    view = Frame(root) # assign to root of application -- master
-    view.pack()
-    new_image = ImageTk.PhotoImage(Image.open("test_img.jpg"))
-    if (img_view != None):
-        img_view.configure(image=new_image)
-        img_view.image = new_image
-    outimg = Label(view, text="IMAGE HERE",  image=new_image).pack()
+    display_img()
+#    googlevision("test_img.jpg") # analyze
 
 if __name__ == "__main__":
     main()
